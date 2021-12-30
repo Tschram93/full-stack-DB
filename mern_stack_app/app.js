b87733d53c0c6801427a77dca42920da3d7f1a04
TS93 <tschram93@yahoo.com>
2021-12-30 15:39:43 -0500

    addint routes

--- mern_stack_app/app.js
+++ mern_stack_app/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 4000;
+
 const app = express();
-app.get('/', (req, res) => res.send('Response is working')); 
-const PORT = process.env.PORT || 4000;
+connectDB();
 
-// app.use();
-
-app.listen(PORT, () => {
-    console.log(`Server is running on localhost:${PORT}`);
-})
+app.get('/', (req, res) => res.send('Response is working!'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
